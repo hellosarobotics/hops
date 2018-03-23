7724e0d98810f05d38cb381f9ee59f617dfce434
andrea <hellosarobotics@gmail.com>
2018-03-23 15:02:39 +0100

    Aggiunta una terminazione per il ciclo While. Prima era While(true)...

--- src/main/java/com/sarobotics/hops/Main.java
+++ src/main/java/com/sarobotics/hops/Main.java
@@ -13,13 +13,19 @@ public class Main {
 
   public static void main(String... args) {
     Logger log = Logger.getLogger(Main.class);
-
     try {
-
       if (args.length >= 3) {
+
         BMP280 bmp280;
         Action action;
-        int actualAltitude, detachAltitude, openParachuteAltitude, tempoCiclo;
+        /*
+         * actualAltitude: L'altitudine attuale al primo avvio del BMP280
+         * detachAltitude: Altitudine di distacco della sonda
+         * openParachuteAltitude: Altitudine di apertura paracadute
+         * cicleTime: velocita' del ciclo di lettura dal BMP280
+         */
+        int actualAltitude, detachAltitude, openParachuteAltitude, cicleTime;
+        boolean continua = true;
 
         int indiceArgomenti = 0;
         if (args[indiceArgomenti].equalsIgnoreCase("sim")) {
@@ -28,54 +34,61 @@ public class Main {
           detachAltitude = actualAltitude + Integer.parseInt(args[++indiceArgomenti]);
           openParachuteAltitude = actualAltitude + Integer.parseInt(args[++indiceArgomenti]);
           action = new ActionSimulator(detachAltitude, openParachuteAltitude);
-          tempoCiclo = Integer.parseInt(args[++indiceArgomenti]);
+          cicleTime = Integer.parseInt(args[++indiceArgomenti]);
           //action = new ActionHW(detachAltitude, openParachuteAltitude);
         } else {
           bmp280 = new BMP280HW();
           actualAltitude = (int) bmp280.getAltitudeInMeter();
-          //Trick... spesso il bmp280 all'accensione da un errore troppo grande in termini di misura della pressione il seguente if e' un espediente per evitarlo
-          if(actualAltitude>1000) { //HARDCODED ma per i test va bene cosi'
+          // Trick... spesso il bmp280 all'accensione da un errore troppo grande in termini di misura della pressione il seguente if e' un espediente per evitarlo. 
+          // Ovviamente se allo start ci troviamo ad una altitudine > 1000 metri attendiamo comunque 5 secondi e riprendiamo l'altitudine per sicurezza. 
+          if (actualAltitude > 1000) { //HARDCODED ma per i test va bene cosi'
             Thread.sleep(5000);
             actualAltitude = (int) bmp280.getAltitudeInMeter();
           }
           detachAltitude = actualAltitude + Integer.parseInt(args[indiceArgomenti++]);
           openParachuteAltitude = actualAltitude + Integer.parseInt(args[indiceArgomenti++]);
           action = new ActionHW(detachAltitude, openParachuteAltitude);
-          tempoCiclo = Integer.parseInt(args[indiceArgomenti++]);
+          cicleTime = Integer.parseInt(args[indiceArgomenti]);
         }
 
         log.info("Parametri allo start");
-        log.info("Start Altitude: " + actualAltitude + "m. DETACH at: " + detachAltitude + "m. OpenParachute at: " + openParachuteAltitude + "m.");
+        log.info("Start Altitude: " + actualAltitude + "m. DETACH at: " + detachAltitude + "m. OpenParachute at: " + openParachuteAltitude + "m. CicleTime: " + cicleTime + " milliseconds");
 
         final AltitudeController ac = new AltitudeController(actualAltitude);
 
-        while (true) {
-          int altit = (int) bmp280.getAltitudeInMeter();
-            if (altit != 3733) { //Hardcoded controllo su errore altitudine. Forse da imputare al BMP280HW un po' vecchiotto.
-              ac.settaAltitudineAttuale(altit);
-              if (ac.isGoingUp()) {
-                log.info("\t  UP\t\t" + ac.print());
-                if (action.canDetach(ac.getActualAltitude()) && action.getDeveAncoraScoppiare()) {
-                  action.setDeveAncoraScoppiare(false);
-                  action.sganciaSonda();
-                }
-              } else if (ac.isGoingDown()) {
-                log.info("\t DOWN\t\t" + ac.print());
-                if (action.canOpenParachute(ac.getActualAltitude())) {
-                  action.setIlParacaduteSiDeveAncoraAprire(false);
-                  action.apriParacadute();
+          while (continua) {
+            try {
+              int altit = (int) bmp280.getAltitudeInMeter();
+              if (altit != 3733) { //Hardcoded controllo su errore altitudine. Forse da imputare al BMP280HW un po' vecchiotto.
+                ac.settaAltitudineAttuale(altit);
+                if (ac.isGoingUp()) {
+                  log.info("\t  UP\t\t" + ac.print());
+                  if (action.canDetach(ac.getActualAltitude()) && action.getDeveAncoraScoppiare()) {
+                    action.setDeveAncoraScoppiare(false);
+                    action.sganciaSonda();
+                  }
+                } else if (ac.isGoingDown()) {
+                  log.info("\t DOWN\t\t" + ac.print());
+                  if (action.canOpenParachute(ac.getActualAltitude())) {
+                    action.setIlParacaduteSiDeveAncoraAprire(false);
+                    action.apriParacadute();
+                  }
                 }
               }
+              Thread.sleep(cicleTime);
+            }catch (Exception e){
+              continua = false;
+              e.printStackTrace();
+              log.error(e.getMessage());
+            }
           }
-          Thread.sleep(tempoCiclo);
-        }
 
       } else {
         log.info(messaggioParametri());
       }
-    }catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
-      log.error("<-- PROBABILE ERRORE SUI PARAMETRI --> \n"+ messaggioParametri() );
+      log.error("<-- PROBABILE ERRORE SUI PARAMETRI --> \n" + messaggioParametri());
       System.exit(0);
     } catch (InvalidOpenParachuteAltitude invalidOpenParachuteAltitude) {
       invalidOpenParachuteAltitude.printStackTrace();
